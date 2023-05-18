32497161cc9b0c498bddf75bce4e716ee7ae6bfa
Rafał <mysliwczykrafal@gmail.com>
2023-05-18 14:16:29 +0000

    Fixed login rerouting so that app can be served as a subpath on the server

--- submodules.py
+++ submodules.py
@@ -1,10 +1,10 @@
-from flask import redirect, session
+from flask import redirect, session, url_for
 from functools import wraps
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user") is None:
-            return redirect("/login")
+            return redirect(url_for("login"))
         return f(*args, **kwargs)
     return decorated_function
